5f0bc07764330c91689a30041c500b9fabba5073
apetreibogdan <70704260+apetreibogdan@users.noreply.github.com>
2021-12-30 21:45:01 +0200

    Initial Commit

--- src/Models/Room.cs
+++ src/Models/Room.cs
@@ -1,7 +1,19 @@
+using System.Collections.Generic;
+
 namespace HogwartsHouses.Models
 {
     [System.Serializable]
     public class Room
     {
+        public int ID { get; set; }
+
+        public bool IsOcupied { get; set; }
+
+
+
+        public List<Student> Roommates { get; set; }
+
+
+
     }
 }
--- src/Models/Student.cs
+++ src/Models/Student.cs
@@ -3,5 +3,10 @@ namespace HogwartsHouses.Models
     [System.Serializable]
     public class Student
     {
+        public int Id { get; set; }
+
+        
+
+
     }
 }
